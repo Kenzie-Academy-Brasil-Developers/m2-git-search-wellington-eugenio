537599582601ab5fc786311e4a68d28fa9d4a993
wellington-eugenio <wellington.eliteinfo@gmail.com>
2022-10-12 23:07:12 -0300

    rasc integrade

---
+++ rascunhos/rascunhos.js
@@ -0,0 +1,27 @@
+/*
+//cardusuario
+<img class="profile-img" src="../../src/img/profile.png" alt="">
+<div class="profile-desc">
+    <h3 class="profile-name">Nome do Usuario</h3>
+    <p class="profile-job">Profissao</p>
+</div>
+
+//card projeto
+
+<li class="project-container">
+    <h3 class="project-name">nome do projeto</h3>
+    <p class="project-desc">Various versions have evolved over the years, 
+       sometimes by accident,sometimes on purpose
+       injected humour and the like
+    </p>
+    <div class="project-buttons">
+        <a class="button-repo" href="" target="_blank">Repositorio</a>
+        <a class="button-link" href="" target="_blank">Demo</a>
+    </div>
+</li>
+
+// usuarios ja procurados
+<li class="profile-record">
+    <a href=""><img class="img-profile" src="../../src/img/profile.png" alt=""></a>
+</li>
+*/
